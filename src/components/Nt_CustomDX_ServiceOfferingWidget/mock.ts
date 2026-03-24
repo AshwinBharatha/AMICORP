9a566d446a59f850cd3c0a9758f65446fa3555b0
AshwinBharatha <21ag1a6610@gmail.com>
2026-03-24 18:27:53 +0530

    Revert "Second Screen fixes"
    This reverts commit 080da3112f7b71c8e5aeb4e3a98ecfa07e9d404c.

--- src/components/Nt_CustomDX_ServiceOfferingWidget/mock.ts
+++ src/components/Nt_CustomDX_ServiceOfferingWidget/mock.ts
@@ -9,100 +9,51 @@ const configProps = {
     source: [
       {
         name: 'Australia',
-        countryCode: 'AU',
         isMajor: true
       },
       {
         name: 'Canada',
-        countryCode: 'CA',
         isMajor: true
       },
       {
         name: 'Europe',
-        countryCode: 'EU',
         isMajor: true
       },
       {
         name: 'Malaysia',
-        countryCode: 'MY',
         isMajor: true
       },
       {
         name: 'Netherlands',
-        countryCode: 'NL',
         isMajor: true
       },
       {
         name: 'Singapore',
-        countryCode: 'SG',
         isMajor: true
       },
       {
         name: 'Armenia',
-        countryCode: 'AM',
         isMajor: false
       },
       {
         name: 'Austria',
-        countryCode: 'AT',
         isMajor: false
       },
       {
         name: 'Azerbaijan',
-        countryCode: 'AZ',
         isMajor: false
       },
       {
         name: 'Bahamas',
-        countryCode: 'BS',
         isMajor: false
       },
       {
         name: 'Bahrain',
-        countryCode: 'BH',
         isMajor: false
       },
       {
         name: 'Bangladesh',
-        countryCode: 'BD',
         isMajor: false
-      }
-    ],
-    fields: {}
-  },
-  serviceDatasource: {
-    source: [
-      {
-        name: 'Company Management',
-        serviceId: 'CM',
-        iconSource: 'fa-solid fa-building-user',
-        countryCode: 'SG',
-        isActive: true,
-        pyGUID: 'mock-service-1'
-      },
-      {
-        name: 'Financial Markets',
-        serviceId: 'FM',
-        iconSource: 'fa-solid fa-sack-dollar',
-        countryCode: 'SG',
-        isActive: true,
-        pyGUID: 'mock-service-2'
-      },
-      {
-        name: 'Fund Administration',
-        serviceId: 'FA',
-        iconSource: 'fa-solid fa-circle-dollar-to-slot',
-        countryCode: 'MY',
-        isActive: true,
-        pyGUID: 'mock-service-3'
-      },
-      {
-        name: 'Trust Management',
-        serviceId: 'TM',
-        iconSource: 'fa-solid fa-handshake-angle',
-        countryCode: 'CA',
-        isActive: true,
-        pyGUID: 'mock-service-4'
       }
     ],
     fields: {}
